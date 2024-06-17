fe5954b38cfbaaf1b7673ec4662cac01f3bd9536
simararustam <simararustam@gmail.com>
2024-06-17 17:04:50 +0400

    we are happy to share our first commit

---
+++ Model/baseclass.py
@@ -0,0 +1,13 @@
+import uuid
+from datetime import datetime
+
+class Baseclass:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = str(datetime.now())
+        self.updated_at = self.created_at
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+# maybe string representation can be added by Shukufa
