127244c53143069d4dc81f63c9bbfdf58c28e594
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-18 13:25:09 +0400

    run flask app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Welcome to Hbnb project created by legends"
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8081, debug=True)
