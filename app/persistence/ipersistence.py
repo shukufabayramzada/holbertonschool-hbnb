c6aef22a8597b0bd1b952c78670d3db58dffb7ff
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-18 16:59:18 +0400

    solved pycode errors

--- app/persistence/ipersistence.py
+++ app/persistence/ipersistence.py
@@ -11,28 +11,28 @@ Includes method siganturesto handle various data types
 class IPersistenceManager(ABC):
     """
     Interface for defining abstract methods
-    """ 
+    """
     @abstractmethod
     def save(self, entity):
         """
         Save entity to the storage
         """
-        pass    
-    
+        pass
+
     @abstractmethod
     def get(self, entity_id, entity_type):
         """
         Retrieve the entity from the storage
         """
         pass
-    
+
     @abstractmethod
     def update(self, entity):
         """
         Update the entity in the storage
         """
-        pass 
-    
+        pass
+
     @abstractmethod
     def delete(self, entity_id, entity_type):
         """
