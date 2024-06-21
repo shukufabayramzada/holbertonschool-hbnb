77bdd12d449f434a3cd3884b6317d2f2f97333f3
simararustam <simararustam@gmail.com>
2024-06-21 16:56:14 +0400

    update for app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask
-from Api.amenity_controller import amenity_controller
 
 app = Flask(__name__)
 
-app.register_blueprint(amenity_controller)
-
-app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Welcome to Hbnb project created by legends"
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8081, debug=True)
