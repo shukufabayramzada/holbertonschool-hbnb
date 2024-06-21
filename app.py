d105be1cd0f6ae72c643d7f471be4c0be0355427
simararustam <simararustam@gmail.com>
2024-06-21 19:42:07 +0400

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
+from Api.user_controller import user_controller
+from Api.country_city_controller import country_city_controller
+from Api.amenity_controller import amenity_controller
+from Api.place_controller import place_controller
+from Api.review_controller import review_controller
 
 app = Flask(__name__)
 
+app.register_blueprint(user_controller)
+app.register_blueprint(country_city_controller)
+app.register_blueprint(amenity_controller)
+app.register_blueprint(place_controller)
+app.register_blueprint(review_controller)
 
 @app.route('/')
 def hello():
-    return "Welcome to Hbnb project created by legends"
-
+    return "Welcome to Hbnb part 1"
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8081, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
