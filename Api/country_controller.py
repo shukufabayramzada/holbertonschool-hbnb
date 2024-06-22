f174527c3b741f8c098cb64d96ada2666281b35e
simararustam <simararustam@gmail.com>
2024-06-22 15:23:48 +0400

    idk what i do

--- Api/country_controller.py
+++ Api/country_controller.py
@@ -2,13 +2,15 @@ from flask import Flask, request, jsonify
 from Persistence.data_manager import DataManager
 from Model.country import Country
 
-app = Flask(__name__)
+coutry_controller = Flask(__name__)
 data_manager = DataManager()
 
-@app.route('/country', methods=['POST'])
+@coutry_controller.route('/country', methods=['POST'])
 def post_country():
     data = request.get_json()
-    country = Country(data['id'], data['name'], data['description'])
+    country_id = data.get('coutry_id')
+    name = data.get('name')
+    country = Country(country_id=country_id, name=name)
     # save the country to the database
     data_manager.save(country)
     # print country for now
@@ -16,7 +18,7 @@ def post_country():
     return jsonify(country.__dict__), 201 
 
 
-@app.route('/countries/<country_id>', methods=['GET'])
+@coutry_controller.route('/countries/<country_id>', methods=['GET'])
 def get_country(country_id):
     country = data_manager.get(entity_id=country_id, entity_type='Country')
     return jsonify(country.__dict__), 200
--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from Api.user_controller import user_controller
+from Api.city_controller import city_control
 from Api.country_city_controller import country_city_controller
+from Api.country_controller import coutry_controller
 from Api.amenity_controller import amenity_controller
 from Api.place_controller import place_controller
 from Api.review_controller import review_controller
 
 app = Flask(__name__)
 
-app.register_blueprint(user_controller)
-app.register_blueprint(country_city_controller)
-app.register_blueprint(amenity_controller)
-app.register_blueprint(place_controller)
-app.register_blueprint(review_controller)
+# app.register_blueprint(user_controller)
+# app.register_blueprint(country_city_controller)
+# app.register_blueprint(coutry_controller)
+app.register_blueprint(city_control)
+# app.register_blueprint(amenity_controller)
+# app.register_blueprint(place_controller)
+# app.register_blueprint(review_controller)
 
 @app.route('/')
 def hello():
